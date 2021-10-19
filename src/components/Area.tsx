7af25d175fabfc0491a8539a4a6ee8c6bd4d3c13
sarkarChandrima <sarkarchandrima1@gmail.com>
2021-10-19 18:29:16 +0530

    new components added

---
+++ src/components/Area.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Profile from './Profile'
+import Work from './Work'
+import Contact from './Contact'
+
+interface AreaProps {
+    areaNo: number;
+  }
+
+const Area: React.FC<AreaProps> = (props:AreaProps)=> {
+    if(props.areaNo===0){
+        return (
+            <Profile/>
+        )
+    }
+    if(props.areaNo===1){
+        return (
+            <Work/>
+        )
+    }
+    if(props.areaNo===2){
+        return (
+            <Contact/>
+        )
+    }
+    return null
+}
+export default Area
